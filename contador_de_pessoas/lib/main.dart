c7b052a0af156dd060274885a3a064ef30083794
Rafael Carvalho Itajahy <rafaelitajahy@gmail.com>
2019-07-16 16:20:09 -0300

    Projetos contador de pessoas finalizado

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -4,32 +4,80 @@ void main(){
   runApp(MaterialApp(
     title: "Contador de Pessoas",
     debugShowCheckedModeBanner: false,
-    home: Column(
+    home: Home() 
+  ));  
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+
+  void _changePeople(int delta){
+    setState(() {
+       _people += delta;
+
+       if (_people < 0) {
+          _infoText = "Mundo invertido?!";           
+       } else if (_people <= 10){
+         _infoText = "Pode entrar!";
+       } else {
+         _infoText = "Lotado!";
+       }
+    });   
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+      Image.asset(
+        "images/restaurant.jpg",
+        fit: BoxFit.cover,
+        height: 1000.0,
+      ),  
+      Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text("Pessoas: 0", 
+        Text("Pessoas: $_people", 
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
-              onPressed: (){},
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child:FlatButton(
+                  child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
+                  onPressed: (){
+                    _changePeople(1);
+                  },
+                ),
             ),
-            FlatButton(
-              child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
-              onPressed: (){},
+              Padding(
+              padding: EdgeInsets.all(10.0),
+              child:FlatButton(
+                  child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white),),
+                  onPressed: (){
+                    _changePeople(-1);
+                  },
+                ),
             ),
-
           ],
         ),
-        Text("Pode Entrar!", 
+        Text(_infoText, 
         style: TextStyle(color: Colors.white, 
             fontStyle: FontStyle.italic,
             fontSize: 30.0),
         ),
       ],
     ), 
-  ));  
+      ],
+    );
+  }
 }
